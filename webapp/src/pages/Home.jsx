5ddaf48de4e69ab81f239a6228886e86bd25def8
Взрослая академия <reshetov@mail.ru>
2026-02-19 20:35:21 +0300

    dev: shadc-ui + routing

---
+++ webapp/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id iure aliquid
+        voluptate!
+      </div>
+    </div>
+  );
+}
